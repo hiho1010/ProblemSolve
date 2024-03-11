a7407100a59c404800bc8dffa6aec289760afda8
Lim HeeHo <121926482+hiho1010@users.noreply.github.com>
2024-03-11 11:13:04 +0900

    [level 0] Title: 문자열의 앞의 n글자, Time: 0.02 ms, Memory: 83.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+class Solution {
+    public String solution(String my_string, int n) {
+        int len = my_string.length();
+        
+        String subStr = my_string.substring(0, n);
+
+        return subStr;
+    }
+}
