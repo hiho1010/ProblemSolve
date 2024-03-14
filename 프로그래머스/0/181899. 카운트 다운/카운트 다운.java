5f4c18f467062f419ca03f36a1bf862c233187af
Lim HeeHo <121926482+hiho1010@users.noreply.github.com>
2024-03-14 10:16:30 +0900

    [level 0] Title: 카운트 다운, Time: 0.02 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int start, int end_num) {
+        int len = start - end_num+1;
+        
+        
+        int[] answer = new int[len];
+        
+        for (int i= 0; i < len;i++ ) {
+            answer[i] = start - i;
+        }
+        
+        
+        return answer;
+    }
+}
