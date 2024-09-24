8852ecac894523e97e83c878be56117fddca0d64
Lim HeeHo <121926482+hiho1010@users.noreply.github.com>
2024-09-24 23:20:49 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 104 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -1,20 +1,28 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		String str = sc.next();
-		sc.close();
-		int len = str.length();
-		int ans = 1;
-		
-		for(int i=0;i<len/2;i++) {
-			if(str.charAt(i)!=str.charAt(len-1-i)) {
-				ans = 0;
-			}
-		}
-		System.out.println(ans);
-		
-	}
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static String text;
+    static int n;
+    static int out = 1;
+
+    public static void main(String[] args) throws IOException {
+        text = br.readLine();
+        n = text.length();
+        int check = n / 2;
+
+        for (int i = 0; i < check; i++) {
+            int prev = text.charAt(i);
+            int next = text.charAt(n - i - 1);
+            
+            if (prev != next) {
+                out = 0;
+                break;
+            }
+        }
+
+        System.out.println(out);
+    }
 }
