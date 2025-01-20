f69226a1b612658d7c1af5a69921450a76ef7bad
Lim HeeHo <121926482+hiho1010@users.noreply.github.com>
2025-01-20 23:07:40 +0900

    [Silver II] Title: DFS와 BFS, Time: 276 ms, Memory: 24496 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,72 +1,80 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static int N, M, V;
-    static List<List<Integer>> graph;
-    static boolean[] visited;
+	static StringBuilder sb = new StringBuilder();
+	static boolean[] check;
+	static int[][] arr;
+	
+	static int node, line, start;
+	
+	static Queue<Integer> q = new LinkedList<>();
 
-    public static void main(String[] args) throws Exception {
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        V = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		node = Integer.parseInt(st.nextToken());
+		line = Integer.parseInt(st.nextToken());
+		start= Integer.parseInt(st.nextToken());
+		
+		arr = new int[node+1][node+1];
+		check = new boolean[node+1];
+		
+		for(int i = 0 ; i < line ; i ++) {
+			StringTokenizer str = new StringTokenizer(br.readLine());
+			
+			int a = Integer.parseInt(str.nextToken());
+			int b = Integer.parseInt(str.nextToken());
+			
+			arr[a][b] = arr[b][a] =  1;	
+		}
+			//sb.append("\n");
+			dfs(start);
+			sb.append("\n");
+			check = new boolean[node+1];
+			
+			bfs(start);
+			
+			System.out.println(sb);
+		
+		}
+	public static void dfs(int start) {
+		
+		check[start] = true;
+		sb.append(start + " ");
+		
+		for(int i = 0 ; i <= node ; i++) {
+			if(arr[start][i] == 1 && !check[i])
+				dfs(i);
+		}
+		
+	}
+	
+	public static void bfs(int start) {
+		q.add(start);
+		check[start] = true;
+		
+		while(!q.isEmpty()) {
+			
+			start = q.poll();
+			sb.append(start + " ");
+			
+			for(int i = 1 ; i <= node ; i++) {
+				if(arr[start][i] == 1 && !check[i]) {
+					q.add(i);
+					check[i] = true;
+				}
+			}
+		}
+		
+		
+	}
 
-        graph = new ArrayList<>();
-        for (int i = 0; i <= N; i++) {
-            graph.add(new ArrayList<>());
-        }
-
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
-            graph.get(from).add(to);
-            graph.get(to).add(from);
-        }
-
-        for (int i = 1; i <= N; i++) {
-            Collections.sort(graph.get(i));
-        }
-
-        visited = new boolean[N + 1];
-        dfs(V);
-        System.out.println();
-
-        visited = new boolean[N + 1];
-        bfs(V);
-    }
-
-    static void dfs(int node) {
-        visited[node] = true;
-        System.out.print(node + " ");
-
-        for (int next : graph.get(node)) {
-            if (!visited[next]) {
-                dfs(next);
-            }
-        }
-    }
-
-    static void bfs(int start) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(start);
-        visited[start] = true;
-
-        while (!queue.isEmpty()) {
-            int node = queue.poll();
-            System.out.print(node + " ");
-
-            for (int next : graph.get(node)) {
-                if (!visited[next]) {
-                    visited[next] = true;
-                    queue.add(next);
-                }
-            }
-        }
-    }
 }
