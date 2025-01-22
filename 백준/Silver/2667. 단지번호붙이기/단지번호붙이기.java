7856da491c480fef29bca2476028c28fe64e878f
Lim HeeHo <121926482+hiho1010@users.noreply.github.com>
2025-01-22 23:55:45 +0900

    [Silver I] Title: 단지번호붙이기, Time: 108 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static List<Integer> complexes = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        map = new int[N][N];
+        visited = new boolean[N][N];
+
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < N; j++) {
+                map[i][j] = line.charAt(j) - '0';
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] == 1 && !visited[i][j]) {
+                    int houseCount = dfs(i, j);
+                    complexes.add(houseCount);
+                }
+            }
+        }
+
+        Collections.sort(complexes);
+        System.out.println(complexes.size());
+        for (int count : complexes) {
+            System.out.println(count);
+        }
+    }
+
+    static int dfs(int x, int y) {
+        visited[x][y] = true;
+        int count = 1;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx >= 0 && ny >= 0 && nx < N && ny < N) {
+                if (map[nx][ny] == 1 && !visited[nx][ny]) {
+                    count += dfs(nx, ny);
+                }
+            }
+        }
+
+        return count;
+    }
+}
