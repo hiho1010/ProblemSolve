65446d53acac9a0cddc2c2011868a756a3a605da
Lim HeeHo <121926482+hiho1010@users.noreply.github.com>
2025-01-25 23:56:37 +0900

    [Silver III] Title: 터렛, Time: 108 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Silver/1002. 터렛/터렛.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int t = 0; t < T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int r1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            int r2 = Integer.parseInt(st.nextToken());
+
+            sb.append(findIntersectionCount(x1, y1, r1, x2, y2, r2)).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    static int findIntersectionCount(int x1, int y1, int r1, int x2, int y2, int r2) {
+        double d = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+        if (d == 0 && r1 == r2) {
+            return -1;
+        }
+        else if (d == r1 + r2 || d == Math.abs(r1 - r2)) {
+            return 1;
+        }
+        else if (Math.abs(r1 - r2) < d && d < r1 + r2) {
+            return 2;
+        }
+        else {
+            return 0;
+        }
+    }
+}
