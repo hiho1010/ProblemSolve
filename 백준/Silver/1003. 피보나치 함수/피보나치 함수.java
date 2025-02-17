e8c510346fa80021bf5f30d911a9a602165947cf
Lim HeeHo <121926482+hiho1010@users.noreply.github.com>
2025-02-18 01:08:15 +0900

    [Silver III] Title: 피보나치 함수, Time: 100 ms, Memory: 14088 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -1,37 +1,26 @@
-import java.util.Scanner;
- 
+import java.io.*;
+
 public class Main {
- 
-	static Integer[][] dp = new Integer[41][2];
-	
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		
-		dp[0][0] = 1;	// N=0 일 때의 0 호출 횟수
-		dp[0][1] = 0;	// N=0 일 때의 1 호출 횟수
-		dp[1][0] = 0;	// N=1 일 때의 0 호출 횟수
-		dp[1][1] = 1;	// N=1 일 때의 1 호출 횟수
-		
-		int T = in.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         
-		while(T-- > 0){
-			int N = in.nextInt();
-			fibonacci(N);
-			System.out.println(dp[N][0] + " " + dp[N][1]);
-		}
-		
-	}
-	
-	static Integer[] fibonacci(int N) {
-		// N에 대한 0, 1의 호출 횟수가 없을 떄(탐색하지 않은 값일 때)
-		if(dp[N][0] == null || dp[N][1] == null) {
-			// 각 N에 대한 0 호출 횟수와 1 호출 횟수를 재귀호출한다.
-			dp[N][0] = fibonacci(N - 1)[0] + fibonacci(N - 2)[0];
-			dp[N][1] = fibonacci(N - 1)[1] + fibonacci(N - 2)[1];
-		}
-		// N에 대한 0과 1, 즉 [N][0]과 [N][1] 을 담고있는 [N]을 반환한다.
-		return dp[N];
- 
-	}
- 
+        int T = Integer.parseInt(br.readLine());
+        int[][] dp = new int[41][2];
+        
+        dp[0][0] = 1; dp[0][1] = 0;
+        dp[1][0] = 0; dp[1][1] = 1;
+        
+        for (int i = 2; i <= 40; i++) {
+            dp[i][0] = dp[i - 1][0] + dp[i - 2][0];
+            dp[i][1] = dp[i - 1][1] + dp[i - 2][1];
+        }
+        
+        while (T-- > 0) {
+            int N = Integer.parseInt(br.readLine());
+            sb.append(dp[N][0]).append(" ").append(dp[N][1]).append("\n");
+        }
+        
+        System.out.print(sb);
+    }
 }
