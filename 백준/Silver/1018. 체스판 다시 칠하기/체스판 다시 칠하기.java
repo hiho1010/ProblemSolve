da5adacc187013fa5403b7c55b152a90b59e60c0
HeeHo Lim <121926482+hiho1010@users.noreply.github.com>
2025-08-09 06:32:40 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 108 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -1,45 +1,62 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static char[][] board;
     static int N, M;
-    
+    static int min = Integer.MAX_VALUE;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] sizes = br.readLine().split(" ");
-        N = Integer.parseInt(sizes[0]);
-        M = Integer.parseInt(sizes[1]);
-        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
         board = new char[N][M];
         for (int i = 0; i < N; i++) {
-            board[i] = br.readLine().toCharArray();
+            String line = br.readLine();
+            for (int j = 0; j < M; j++) {
+                board[i][j] = line.charAt(j);
+            }
         }
-        
-        int minRepaints = Integer.MAX_VALUE;
+
         for (int i = 0; i <= N - 8; i++) {
             for (int j = 0; j <= M - 8; j++) {
-                minRepaints = Math.min(minRepaints, getRepaintCount(i, j));
+                findMin(i, j);
             }
         }
-        
-        System.out.println(minRepaints);
+
+        System.out.println(min);
     }
-    
-    static int getRepaintCount(int x, int y) {
-        int repaints1 = 0; // 시작이 'B'인 경우
-        int repaints2 = 0; // 시작이 'W'인 경우
-        char[] colors = {'B', 'W'};
-        
+
+    static void findMin(int startRow, int startCol) {
+        int cntW = 0;
+        int cntB = 0;
+
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                if (board[x + i][y + j] != colors[(i + j) % 2]) {
-                    repaints1++;
+                int currentRow = startRow + i;
+                int currentCol = startCol + j;
+
+                if ((i + j) % 2 == 0) {
+                    if (board[currentRow][currentCol] != 'W') {
+                        cntW++;
+                    }
+                    if (board[currentRow][currentCol] != 'B') {
+                        cntB++;
+                    }
                 }
-                if (board[x + i][y + j] != colors[(i + j + 1) % 2]) {
-                    repaints2++;
+                else {
+                    if (board[currentRow][currentCol] != 'B') {
+                        cntW++;
+                    }
+                    if (board[currentRow][currentCol] != 'W') {
+                        cntB++;
+                    }
                 }
             }
         }
-        return Math.min(repaints1, repaints2);
+
+        min = Math.min(min, Math.min(cntW, cntB));
     }
 }
