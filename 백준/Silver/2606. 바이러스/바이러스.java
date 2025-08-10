20669d74f7478d7e44fea260ab307a332b3a19f9
HeeHo Lim <121926482+hiho1010@users.noreply.github.com>
2025-08-10 23:37:21 +0900

    [Silver III] Title: 바이러스, Time: 112 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,56 +1,39 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-
-    static int V, E;
-    static boolean[] visited;
-    static ArrayList<Integer> tracks = new ArrayList<>();
-    static int startNode = 1;
-    static ArrayList<Integer>[] adj;
-
-    static int cnt;
+    static int cnt, V, E;
+    static HashSet<Integer> visited = new HashSet<>();
+    static ArrayDeque<Integer> stack = new ArrayDeque<>(Arrays.asList(1));
+    static int[][] adj;
 
     public static void main(String[] args) throws IOException {
-
         V = Integer.parseInt(br.readLine());
         E = Integer.parseInt(br.readLine());
-        visited = new boolean[V + 1];
+        adj = new int[V+1][V+1];
 
-        adj = new ArrayList[V+1];
-        for (int i = 0; i < V+1; i++) {
-            adj[i] = new ArrayList<>();
-        }
-
-        for (int i = 0; i < E; i++) { // E번 반복해야 함
+        for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-            adj[start].add(end);
-            adj[end].add(start);
+            adj[start][end] = adj[end][start] = 1;
         }
 
-        ArrayDeque<Integer> stack = new ArrayDeque<>(List.of(startNode));
-        visited[startNode] = true; // 시작 노드 방문 체크
-
-        while (!stack.isEmpty()) { // tracks.isEmpty() -> stack.isEmpty()로 수정
+        while (!stack.isEmpty()) {
             int cur = stack.removeLast();
-            tracks.add(cur); // 방문한 노드를 tracks 리스트에 추가
 
-            for (Integer destination : adj[cur]) {
-                if (!visited[destination]) {
-                    visited[destination] = true; // 방문한 노드로 체크
-                    stack.addLast(destination);
-                    cnt++;
+            if (!visited.contains(cur)) {
+                visited.add(cur);
+            }
+
+            for (int nxt = 0; nxt < V + 1; nxt++) {
+                if (!visited.contains(nxt) && adj[cur][nxt] == 1) {
+                    stack.add(nxt);
                 }
             }
         }
-
-        System.out.println(cnt);
+        System.out.println(visited.size()-1);
     }
 }
