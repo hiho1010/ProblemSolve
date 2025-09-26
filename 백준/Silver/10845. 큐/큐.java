5b37e969ace96362cc5ff220e475b2ac52044d30
HeeHo Lim <121926482+hiho1010@users.noreply.github.com>
2025-09-26 23:43:52 +0900

    [Silver IV] Title: 큐, Time: 196 ms, Memory: 19556 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static ArrayList<Integer> queue = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+
+            switch (command) {
+                case "push":
+                    int X = Integer.parseInt(st.nextToken());
+                    queue.add(X);
+                    break;
+                case "pop":
+                    if (queue.isEmpty()) {
+                        bw.write("-1\n");
+                    } else {
+                        bw.write(queue.remove(0) + "\n");
+                    }
+                    break;
+                case "size":
+                    bw.write(queue.size() + "\n");
+                    break;
+                case "empty":
+                    if (queue.isEmpty()) {
+                        bw.write("1\n");
+                    } else {
+                        bw.write("0\n");
+                    }
+                    break;
+                case "front":
+                    if (queue.isEmpty()) {
+                        bw.write("-1\n");
+                    } else {
+                        bw.write(queue.get(0) + "\n");
+                    }
+                    break;
+                case "back":
+                    if (queue.isEmpty()) {
+                        bw.write("-1\n");
+                    } else {
+                        bw.write(queue.get(queue.size() - 1) + "\n");
+                    }
+                    break;
+            }
+        }
+        bw.flush();
+        bw.close();
+    }
+}
